ba86990c4808c0aa5b5e9452cc416d4402edb1cb
Muhammad Ashar Ansari <144896935+Muhammad-Ashar-Ansari@users.noreply.github.com>
2026-06-17 16:21:12 +0500

    Add files via upload
    Initial ESP32 Blink Test Completed
    
    Added ESP32 Serial Communication Test
    
    Successfully Tested ESP32 WiFi Connectivity
    
    Added ESP32-CAM Live Video Streaming Test

---
+++ Software/ESP32 Test/ESP32_Wifi_Test/ESP32_Wifi_Test.ino.ino
@@ -0,0 +1,23 @@
+#include <WiFi.h>
+
+const char* ssid = "TumharaWiFiName";      // Apna WiFi naam likho
+const char* password = "TumharaPassword";   // Apna WiFi password
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("\nConnected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+
+}
